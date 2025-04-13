6da72c941d9c11dadf349710e8a58ef84f2c394b
bkdra <wgg0721@gmail.com>
2025-04-13 22:58:07 +0800

    final

--- final/TestFile_Lab1_2025/OwnTest1.java
+++ final/TestFile_Lab1_2025/OwnTest1.java
@@ -1,11 +1,12 @@
 // used to test invalid string
 public class OwnTest1 {
     public static void main() {
-        String a = 'test1';
-        String b = "test2\";
-        String c = "tes"t3";
-        String d = "test4;
-        String e = "test5";
+        string o = "te\"st";
+        string a = 'test1';
+        string b = "test2\";
+        string c = "tes"t3";
+        string d = "test4;
+        string e = "test5";
     }
 
 }
--- test1.java
+++ test1.java
@@ -6,8 +6,12 @@ s
 d
 fsdf
 */
--1-123=2
--1-2
++1+123=2
+"3122\"54"
+//sdfsd
++1.1232+12354.5465E123f
+int _adfgksdlfhjgsedklfghawoeritsdfoighjsdofighsodfuigyhaoeigfuwaretqt$$$_dfkjhhdfgkj = 0;/******sdfsdf
+2425435435dfbxdfhg******/
 //sdfsdf
 "sdfs"df"
 
